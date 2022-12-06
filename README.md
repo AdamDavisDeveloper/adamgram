a4601db9a6afdb1bd191d532c6afb9d29554e196
Adam Davis <adavisdeveloper@gmail.com>
2022-12-05 20:08:20 -0500

    Some more UI work!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # adamgram
+
+## Instagram for those who hate Instagram but want to share photos similarly
+
+This project was inspired by and borrowed code heavily from the <a href="https://github.com/hunterirving/Huntergram">Huntergram</a> project by Hunter Irving. He was kind enough to allow me to use his code and build on it for my own use. Thank you Hunter!
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import Logo from './assets/logo.svg'
+import { useState } from 'react';
+import Logo from './assets/logo.svg';
+import ProfilePhoto from './assets/profile.jpg';
+import BlueCheck from './assets/blue-check.svg';
+
 import './Main.scss'
 
 function App() {
   return (
     <div id="App">
-      <div className="logo-container">
+      <div className="logo-wrapper">
         <img src={Logo} alt="Adamgram logo" />
       </div>
 
       <div id="BioWrapper">
-        
+        <img className="profile-photo" src={ProfilePhoto} alt="" />
+        <div className="bio-info-wrapper">
+          <h2 className="user-name">adam</h2>
+          <img id="VerifiedCheck" src={BlueCheck} alt="verified blue checkmark" />
+          <div id="Buttons">
+            <div id="message_button" className="button" onClick={(e) => null}>
+							Message
+						</div>
+            <div id="follow_button" className="button" onClick={(e) => null}>
+              Follow
+            </div>
+          </div>
+        </div>
+
+        <div id="DescriptionContainer">
+					<div id="description">
+						a human from earth
+						<br />
+						<a href="https://github.com/hunterirving/Huntergram">github.com/hunterirving/Huntergram</a>
+					</div>
+				</div>
       </div>
     </div>
   )
