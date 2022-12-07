a87364b4924da314d4c9cd4550002e9b99cf85f0
Adam Davis <adavisdeveloper@gmail.com>
2022-12-07 18:48:46 -0500

    add in diff images

--- src/DummyData.tsx
+++ src/DummyData.tsx
@@ -1,16 +1,49 @@
+import FogStreet from './assets/content/fog-street.png';
+import Ivy from './assets/content/ivy.png';
 import GoneFishin from './assets/content/gone_fishin.jpg';
 import Lantern from './assets/content/lantern.jpg';
+import Moon from './assets/content/moon.jpg';
+import Prometheus from './assets/content/prometheus.jpg';
+import Tunnel from './assets/content/tunnel.jpg';
 
 export default [
     {
-        imgName: GoneFishin,
+        imgName: FogStreet,
         description: "Gone fishing!",
         date: "10-14-22",
         likes: 0,
         comments: [],
     },
     {
-        imgName: Lantern,
+        imgName: Ivy,
+        description: "A lantern",
+        date: "11-16-22",
+        likes: 0,
+        comments: [],
+    },
+    {
+        imgName: Moon,
+        description: "A lantern",
+        date: "11-16-22",
+        likes: 0,
+        comments: [],
+    },
+    {
+        imgName: Prometheus,
+        description: "A lantern",
+        date: "11-16-22",
+        likes: 0,
+        comments: [],
+    },
+    {
+        imgName: Tunnel,
+        description: "A lantern",
+        date: "11-16-22",
+        likes: 0,
+        comments: [],
+    },
+    {
+        imgName: GoneFishin,
         description: "A lantern",
         date: "11-16-22",
         likes: 0,
@@ -23,37 +56,4 @@ export default [
         likes: 0,
         comments: [],
     },
-    {
-        imgName: Lantern,
-        description: "A lantern",
-        date: "11-16-22",
-        likes: 0,
-        comments: [],
-    },
-    {
-        imgName: Lantern,
-        description: "A lantern",
-        date: "11-16-22",
-        likes: 0,
-        comments: [],
-    },
-    {
-        imgName: Lantern,
-        description: "A lantern",
-        date: "11-16-22",
-        likes: 0,
-        comments: [],
-    },
-    {
-        imgName: Lantern,
-        description: "A lantern",
-        date: "11-16-22",
-        likes: 0,
-        comments: [],
-    },
-]
-
-/* 
-We need to find a way to split up our data into rows which contain 3 photos.
-This will allow us to map over 3 at a time and generate a row with only three photos
-*/
+];
