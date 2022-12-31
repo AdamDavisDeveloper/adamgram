e56aedbaa0b9683432512ec47954faf8bd73ed63
Adam Davis <adavisdeveloper@gmail.com>
2022-12-30 22:51:43 -0500

    delete old

--- src/DummyData.tsx
+++ src/DummyData.tsx
@@ -1,10 +1,5 @@
 import FogStreet from './assets/content/fog-street.png';
 import Ivy from './assets/content/ivy.png';
-import GoneFishin from './assets/content/gone_fishin.jpg';
-import Lantern from './assets/content/lantern.jpg';
-import Moon from './assets/content/moon.jpg';
-import Prometheus from './assets/content/prometheus.jpg';
-import Tunnel from './assets/content/tunnel.jpg';
 import LoganSilly from './assets/content/logan-silly.png';
 import Chopper from './assets/content/chopper.png';
 import AgnesPool from './assets/content/agnes-pool.jpg';
@@ -99,7 +94,7 @@ export default [
     },
     {
         imgName: FogStreet,
-        description: "Gone fishing!",
+        description: "Foggy times on my way to work.",
         date: "10-14-22",
         location: "Atlanta, GA",
         likes: 0,
@@ -108,56 +103,11 @@ export default [
     },
     {
         imgName: Ivy,
-        description: "A lantern",
+        description: "Some ivy growing. idk why",
         date: "11-16-22",
         location: "Atlanta, GA",
         likes: 0,
         comments: [],
         slug: "ivy"
-    },
-    {
-        imgName: Moon,
-        description: "A lantern",
-        date: "11-16-22",
-        location: "Atlanta, GA",
-        likes: 0,
-        comments: [],
-        slug: "moon"
-    },
-    {
-        imgName: Prometheus,
-        description: "A lantern",
-        date: "11-16-22",
-        location: "Atlanta, GA",
-        likes: 0,
-        comments: [],
-        slug: "prometheus"
-    },
-    {
-        imgName: Tunnel,
-        description: "A lantern",
-        date: "11-16-22",
-        location: "Atlanta, GA",
-        likes: 0,
-        comments: [],
-        slug: "tunnel"
-    },
-    {
-        imgName: GoneFishin,
-        description: "A lantern",
-        date: "11-16-22",
-        location: "Atlanta, GA",
-        likes: 0,
-        comments: [],
-        slug: "gone-fishing"
-    },
-    {
-        imgName: Lantern,
-        description: "A lantern",
-        date: "11-16-22",
-        location: "Atlanta, GA",
-        likes: 0,
-        comments: [],
-        slug: "lantern"
     },
 ];
