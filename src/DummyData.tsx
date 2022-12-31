1d4c08b577ca4f06658f84b2038b81fe87b0dd65
Adam Davis <adavisdeveloper@gmail.com>
2022-12-30 22:47:52 -0500

    add new posts!

--- src/DummyData.tsx
+++ src/DummyData.tsx
@@ -7,8 +7,69 @@ import Prometheus from './assets/content/prometheus.jpg';
 import Tunnel from './assets/content/tunnel.jpg';
 import LoganSilly from './assets/content/logan-silly.png';
 import Chopper from './assets/content/chopper.png';
+import AgnesPool from './assets/content/agnes-pool.jpg';
+import ChristinaFujifilm from './assets/content/christina-fujifilm.jpg';
+import IntensePool from './assets/content/intense-pool.jpg';
+import MatthewPool from './assets/content/matthew-pool.jpg';
+import MotorcycleJump from './assets/content/motorcycle-jump.jpg';
+import OllieAndElle_01 from './assets/content/ollie-and-elle-01.jpg';
+import Vivi from './assets/content/vivi.jpg';
 
 export default [
+    {
+        imgName: Vivi,
+        description: "Portrait of Vivian",
+        date: "12-30-22",
+        location: "Atlanta, GA",
+        likes: 0,
+        comments: [],
+        slug: "vivi"
+    },
+    {
+        imgName: OllieAndElle_01,
+        description: "Ollie and his girl El 🤍",
+        date: "12-30-22",
+        location: "Atlanta, GA",
+        likes: 0,
+        comments: [],
+        slug: "ollie-and-elle-01"
+    },
+    {
+        imgName: ChristinaFujifilm,
+        description: "Christina and I taking photos of each other with our Fujifilms. :-)",
+        date: "12-30-22",
+        location: "Atlanta, GA",
+        likes: 0,
+        comments: [],
+        slug: "christina-fujifilm"
+    },
+    {
+        imgName: MatthewPool,
+        description: "Super cool trick shot, what in the world.",
+        date: "12-30-22",
+        location: "Atlanta, GA",
+        likes: 0,
+        comments: [],
+        slug: "matthew-pool"
+    },
+    {
+        imgName: IntensePool,
+        description: "Playing some intense pool at the Christmas party.",
+        date: "12-30-22",
+        location: "Atlanta, GA",
+        likes: 0,
+        comments: [],
+        slug: "intense-pool"
+    },
+    {
+        imgName: AgnesPool,
+        description: "Agnes crushing it at pool.",
+        date: "12-30-22",
+        location: "Atlanta, GA",
+        likes: 0,
+        comments: [],
+        slug: "agnes-pool"
+    },
     {
         imgName: LoganSilly,
         description: "Logan Walker posing after an awesome motocross race.",
@@ -17,6 +78,15 @@ export default [
         likes: 0,
         comments: [],
         slug: "logan-silly"
+    },
+    {
+        imgName: MotorcycleJump,
+        description: "Logan getting some rad air at the moto race.",
+        date: "10-15-22",
+        location: "Up north, GA",
+        likes: 0,
+        comments: [],
+        slug: "motorcycle-jump"
     },
     {
         imgName: Chopper,
